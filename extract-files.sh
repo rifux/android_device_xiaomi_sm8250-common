2334a08e6717040dd60f31b8abc31b3cff68fed2
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-03 03:18:05 +0200

    sm8250-common: Import media codecs configs
    From munch V816.0.2.0.ULMMIXM
    
    Change-Id: Ib0220133b54f2cf465003ffbd40c08a5330982c2

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,8 @@ function blob_fixup() {
     case "${1}" in
         vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib64/libdlbdsservice.so)
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+        vendor/etc/init/init.mi_thermald.rc)
+            sed -i "/seclabel u:r:mi_thermald:s0/d" "${2}"
             ;;
         vendor/lib64/libril-qc-hal-qmi.so)
             sed -i 's|ro.product.vendor.device|ro.vendor.radio.midevice|g' "${2}"
