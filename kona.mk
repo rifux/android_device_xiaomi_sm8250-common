3aa6a6a6febae43d40290de6767f3c0cba14c297
pachadomenic <pachadomenic2010@gmail.com>
2024-09-15 22:40:52 +0300

    sm8250-common: Fix build

--- kona.mk
+++ kona.mk
@@ -152,7 +152,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libutilscallstack.vendor
+    libutilscallstack.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
